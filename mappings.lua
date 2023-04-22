924dcdaf08c10be27e15fc1f9695e3129853e64b
root <root@MTHOME-015367>
2023-04-21 21:25:45 -0300

    initial commit

---
+++ mappings.lua
@@ -0,0 +1,61 @@
+return {
+  n = {
+    ["<leader>r"] = {
+      name = " RustTools"
+    },
+    ["<C-s>"] = {
+      "<cmd>w<cr><esc>",
+      desc = "Save file",
+    },
+    ["<leader>o"] = {
+      'o<Esc>0"_D',
+      desc = "New line below",
+    },
+    ["<leader>O"] = {
+      'O<Esc>0"_D',
+      desc = "New line above",
+    },
+    ["<C-d>"] = {
+      "<C-d>zz",
+    },
+    ["<C-u>"] = {
+      "<C-u>zz",
+    },
+    ["<A-j>"] = {
+      "<cmd>m .+1<cr>==",
+      desc = "Move down",
+    },
+    ["<A-k>"] = {
+      "<cmd>m .-2<cr>==",
+      desc = "Move up",
+    }
+  },
+  i = {
+    ["<C-s>"] = {
+      "<cmd>w<cr><esc>",
+      desc = "Save file",
+    },
+    ["<A-j>"] = {
+      "<esc><cmd>m .+1<cr>==gi",
+      desc = "Move down",
+    },
+    ["<A-k>"] = {
+      "<esc><cmd>m .-2<cr>==gi",
+      desc = "Move up",
+    }
+  },
+  v = {
+    ["<C-s>"] = {
+      "<cmd>w<cr><esc>",
+      desc = "Save file",
+    },
+    ["<A-j>"] = {
+      ":m '>+1<cr><cr>gv=gv",
+      desc = "Move down",
+    },
+    ["<A-k>"] = {
+      ":m '<-2<cr><cr>gv=gv",
+      desc = "Move up",
+    }
+  }
+}
